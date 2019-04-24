dafc78146f7be9e0e3ba60db0e12c7e6deed7b92
Dawid klatkiewicz <klatkiewiczdawid@gmail.com>
2019-04-24 00:14:24 -0400

    Added variable for size

--- dklat/fb-dk.c
+++ dklat/fb-dk.c
@@ -3,10 +3,12 @@
 
 int main(void) {
 
+   const unsigned int size = 10;
+
    // Fancy box
-   for (int y = 0; y < 10; y++) {
-      for (int x = 0; x < 10; x++) {
-         if (y == 0 || x == 0 || x == 9 || y == 9)
+   for (int y = 0; y <= size; y++) {
+      for (int x = 0; x <= size; x++) {
+         if (y == 0 || x == 0 || x == size || y == size)
             printf("O"); 
          else 
             printf(" ");
@@ -17,9 +19,9 @@ int main(void) {
    printf("\n\n");
 
    // SUPER fancy box (really fancy) (like seriously)
-   for (int y = 0; y < 10; y++) {
-      for (int x = 0; x < 10; x++) {
-         if (y == 0 || x == 0 || x == 9 || y == 9 || x == y || x == 9 - y)
+   for (int y = 0; y <= size; y++) {
+      for (int x = 0; x <= size; x++) {
+         if (y == 0 || x == 0 || x == size || y == size || x == y || x == size - y)
             printf("O"); 
          else 
             printf(" ");
